b089bc739a15a029681e680e4ca15a5bdf6a3f80
ChaymaaLabied <chaymaalabied1996@gmail.com>
2024-09-09 11:52:08 +0200

    modif compo page Home simple

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,32 @@
-export default function Home(){
-    return (
-        <h1>Hi there</h1>
-    )
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur SportSee</h1>
+      <nav aria-label="x">
+          <ul className="nav">
+            <li className="nav__item">
+              <NavLink
+                to="/user/12"
+                aria-label="Revenir à l'accueil"
+                className="nav__link"
+              >
+                Karl
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/user/18"
+                aria-label="aller à la page à propos"
+                className="nav__link"
+              >
+                Cecilia
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+    </div>
+  );
 }
