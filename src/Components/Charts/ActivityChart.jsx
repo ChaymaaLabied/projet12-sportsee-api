6380d883666fb6753d2a11b2a6360d6ff21ae9d2
ChaymaaLabied <chaymaalabied1996@gmail.com>
2024-09-18 10:19:24 +0200

    paufiner radarChart( activityChart ) logique et style

--- src/Components/Charts/ActivityChart.jsx
+++ src/Components/Charts/ActivityChart.jsx
@@ -4,37 +4,45 @@ import {
   Radar,
   ResponsiveContainer,
   RadarChart,
-  Legend
+  Legend,
 } from "recharts";
-import "../../style/Charts/activityChart.scss"
+import "../../style/Charts/activityChart.scss";
 
 export default function ActivityChart({ userPerformance }) {
   return (
     <div className="activityChart">
+      {/* Conteneur réactif pour ajuster le graphique à la taille du conteneur parent */}
       <ResponsiveContainer width="100%" height="100%">
         <RadarChart
-          outerRadius="70%" // Ajuste la taille du radar pour éviter le débordement
+          outerRadius="60%" // Ajuste la taille du radar par rapport au conteneur
           data={userPerformance.data.map((elt) => ({
+            // Mappe chaque donnée avec sa description 'kind'
             ...elt,
             kind: userPerformance.kinds[elt.kind],
           }))}
         >
-          <PolarGrid />
+          {/* Grille du radar sans lignes radiales */}
+          <PolarGrid radialLines={false} />
+
+          {/* Axe des angles représentant les types d'activités */}
           <PolarAngleAxis
-            dataKey="kind"
-            stroke="#FFFFFF"
-            fontSize={10}
-            fontWeight={600}
-            tickLine={false}
+            dataKey="kind" // Utilise 'kind' pour nommer chaque angle du radar
+            stroke="#FFFFFF" // Couleur du texte
+            fontWeight={600} // Épaisseur du texte
+            tickLine={false} // Supprime la ligne de tick
             className="polar-angle-axis"
           />
+
+          {/* Zone radar qui montre les performances */}
           <Radar
-            dataKey="value"
-            stroke="#E60000"
-            fill="#E60000"
-            fillOpacity={0.7}
-            legendType="none"
+            dataKey="value" // Utilise 'value' pour tracer la performance
+            stroke="#E60000" // Couleur de la ligne extérieure
+            fill="#E60000" // Couleur de remplissage de la zone
+            fillOpacity={0.7} // Opacité du remplissage
+            legendType="none" // Supprime l'icône dans la légende
           />
+
+          {/* Légende qui accompagne le graphique */}
           <Legend />
         </RadarChart>
       </ResponsiveContainer>
